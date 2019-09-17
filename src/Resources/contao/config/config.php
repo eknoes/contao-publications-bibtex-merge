a6ec618dc204c6988fa294b47744757a57ed2b89
Sönke <me@eknoes.de>
2019-09-17 18:13:57 +0200

    Improve Understandability

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,4 +1,2 @@
 <?php
-
-//$GLOBALS['BE_MOD']['Publication System']['Crossref Fetcher'] = array("callback" => "CrossrefFetcher\CrossrefBackend");
-    $GLOBALS['BE_MOD']['Publication System']['DOI Merge'] = array("callback" => "Eknoes\ContaoPublicationsBibtexMerge\Controller\DOIMerge");
+    $GLOBALS['BE_MOD']['Publication System']['Enhancer'] = array("callback" => "Eknoes\ContaoPublicationsBibtexMerge\Controller\DOIMerge");
